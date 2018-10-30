faf115fc83d46cf936ceb9d28e1f66b76c14db42
Rahul Ahuja <rahulahuja404@gmail.com>
2018-10-31 01:03:05 +0530

    first commit

---
+++ src/main/java/org/kjsce/khabar/KhabarApplication.java
@@ -0,0 +1,12 @@
+package org.kjsce.khabar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhabarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KhabarApplication.class, args);
+    }
+}
