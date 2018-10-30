da15ee5343816f108e47a2a2165778742cfdc983
Rahul Ahuja <rahulahuja404@gmail.com>
2018-10-31 01:06:51 +0530

    made a sample controller to test heroku

---
+++ src/main/java/org/kjsce/khabar/controller/DemoController.java
@@ -0,0 +1,14 @@
+package org.kjsce.khabar.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class DemoController {
+    public ResponseEntity<?> index(){
+        return ResponseEntity.ok("Hello World!!!");
+    }
+}
