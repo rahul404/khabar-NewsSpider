b4f9a25a5ac1710ce1cac404e2af4f4f14d98605
Rahul Ahuja <rahulahuja404@gmail.com>
2018-10-31 01:18:43 +0530

    changed RequestMapping to GetMapping in DemoController

--- src/main/java/org/kjsce/khabar/controller/DemoController.java
+++ src/main/java/org/kjsce/khabar/controller/DemoController.java
@@ -1,12 +1,13 @@
 package org.kjsce.khabar.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@GetMapping("/")
 public class DemoController {
     public ResponseEntity<?> index(){
         return ResponseEntity.ok("Hello World!!!");
